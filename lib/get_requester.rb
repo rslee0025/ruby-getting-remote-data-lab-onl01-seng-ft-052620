bbea656bd925f510c3d0d93eaaf252b8286e3e9c
Rachel S. Lee <rslee0025@gmail.com>
2020-06-14 21:57:22 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,16 +1,30 @@
-require 'open-uri'
+require "pry"
 require 'net/http'
+require 'open-uri'
 require 'json'
+class GetRequester 
+attr_accessor :url 
 
-class GetRequester
-  
-  def initialize(url)
-  end
-  
-  def get_response_body
-    uri = URI.parse(uri)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end 
-  
-end
+
+
+def initialize(url)
+@url = url 
+    end  
+
+
+    def get_response_body 
+uri = URI.parse(url)
+response = Net::HTTP.get_response(uri)
+response.body
+end  
+
+
+    def parse_json 
+    JSON.parse(get_response_body)
+   
+
+    end 
+
+
+
+end 
